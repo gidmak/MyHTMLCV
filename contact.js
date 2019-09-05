b1dbb7888cb27375afab92995e10f726398cc680
GidMak <gideonmarkus@gmail.com>
2019-09-05 18:35:59 +0100

    New features added to the CV using JavaScript and more CSS

---
+++ contact.js
@@ -0,0 +1,9 @@
+﻿const modalControls = (() => {
+    
+    //grab elements and asssign functions on action trigger
+    const modal = document.querySelector('#modal-with-form');
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+    const message = document.querySelector('#message');
+})();
+
